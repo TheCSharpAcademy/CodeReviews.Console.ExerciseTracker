30525d68d2c8eb15010b9c3297faab0c217b195d
furiax <carlmalfliet@proximus.be>
2023-08-02 20:32:32 +0200

    created the exercise model

---
+++ Furiax.ExerciseTracker/ExerciseTracker/Models/ExerciseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ExerciseTracker.Models
+{
+	internal class ExerciseModel
+	{
+        public int ExerciseId { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public TimeSpan Duration => DateEnd - DateStart;
+        public string Comments { get; set; }
+    }
+}
