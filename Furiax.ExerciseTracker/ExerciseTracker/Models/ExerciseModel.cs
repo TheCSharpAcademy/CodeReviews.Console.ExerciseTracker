be8b7b712968c7d1f63c23f0c108be90a15c866f
furiax <carlmalfliet@proximus.be>
2023-08-02 21:56:33 +0200

    added all the necessary pages

--- Furiax.ExerciseTracker/ExerciseTracker/Models/ExerciseModel.cs
+++ Furiax.ExerciseTracker/ExerciseTracker/Models/ExerciseModel.cs
@@ -1,12 +1,18 @@
-﻿namespace ExerciseTracker.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseTracker.Models
 {
 	internal class ExerciseModel
 	{
-        public int ExerciseId { get; set; }
-        public string ExerciseName { get; set; }
-        public DateTime DateStart { get; set; }
-        public DateTime DateEnd { get; set; }
-        public TimeSpan Duration => DateEnd - DateStart;
-        public string Comments { get; set; }
-    }
+		public int ExerciseId { get; set; }
+		public string ExerciseType { get; set; }
+		public DateTime DateStart { get; set; }
+		public DateTime DateEnd { get; set; }
+		public TimeSpan Duration => DateEnd - DateStart;
+		public string? Comments { get; set; }
+	}
 }
