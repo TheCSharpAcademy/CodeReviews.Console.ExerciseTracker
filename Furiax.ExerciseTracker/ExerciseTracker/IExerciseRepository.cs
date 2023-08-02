db5f5bbc1d9cf7f0799c149f2ec320e31576fae9
furiax <carlmalfliet@proximus.be>
2023-08-02 22:01:10 +0200

    created the interface repository

---
+++ Furiax.ExerciseTracker/ExerciseTracker/IExerciseRepository.cs
@@ -0,0 +1,13 @@
+﻿using ExerciseTracker.Models;
+
+namespace ExerciseTracker
+{
+	internal interface IExerciseRepository
+	{
+		ExerciseModel GetExerciseById(int id);
+		IEnumerable<ExerciseModel> GetAllExercises();
+		void AddExercise(ExerciseModel exercise);
+		void UpdateExercise(ExerciseModel exercise);
+		void DeleteExercise(int id);
+	}
+}
