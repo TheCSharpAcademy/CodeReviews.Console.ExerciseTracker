3ee63bca9b91421579924281377cc4e16fac0169
JsPeanut <cacahuatearg223@outlook.com>
2023-08-27 16:38:37 -0300

    Codacy issues fix

--- ExerciseTracker.JsPeanut/ExerciseTracker.JsPeanut/Migrations/20230727173407_initialmigration.Designer.cs
+++ ExerciseTracker.JsPeanut/ExerciseTracker.JsPeanut/Migrations/20230727173407_initialmigration.Designer.cs
@@ -16,7 +16,7 @@ namespace ExerciseTracker.JsPeanut.Migrations
     partial class initialmigration
     {
         /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ExerciseTracker.JsPeanut/ExerciseTracker.JsPeanut/Validator.cs
+++ ExerciseTracker.JsPeanut/ExerciseTracker.JsPeanut/Validator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;xcvcvxcxv
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
 namespace ExerciseTracker.JsPeanut
 {
