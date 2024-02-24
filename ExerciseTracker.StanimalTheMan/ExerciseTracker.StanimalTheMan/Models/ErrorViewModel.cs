c6382f2d01d6c89ecfe50fc96045ad5cd8c630d2
Stan Choi <sjc591@nyu.edu>
2024-02-23 20:03:59 -0500

    Init project

---
+++ ExerciseTracker.StanimalTheMan/ExerciseTracker.StanimalTheMan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExerciseTracker.StanimalTheMan.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
