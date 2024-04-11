38bb9775adfa3b631536e7c83ea7fe32929b70e0
Anton de Regt <antonderegt@pm.me>
2024-04-11 21:51:21 +0200

    Fix naming

--- ExerciseTracker.antonderegt/ExerciseContext.cs
+++ ExerciseTracker.antonderegt/ExerciseContext.cs
@@ -7,15 +7,15 @@ namespace ExerciseTracker;
 public class ExerciseContext : DbContext
 {
     public DbSet<Exercise> Exercises { get; set; }
-    private string connectionString { get; set; }
+    private string _connectionString { get; set; }
 
     public ExerciseContext(IConfiguration configuration)
     {
-        connectionString = configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+        _connectionString = configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(connectionString);
+        optionsBuilder.UseSqlServer(_connectionString);
     }
 }
