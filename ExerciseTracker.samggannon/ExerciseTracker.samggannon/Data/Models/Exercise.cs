77e98c47a63cff8373c1253faca5453cca86dbaa
Samuel <Sam.Gannon@sportssouth.biz>
2024-05-13 16:27:04 -0500

    // file restructuring

---
+++ ExerciseTracker.samggannon/ExerciseTracker.samggannon/Data/Models/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseTracker.samggannon.Data.Models
+{
+    internal class Exercise
+    {
+        public int Id { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Comments { get; set; }
+
+    }
+}
