08777584a2da4d8126ef1fb55174cde81fe50968
Samuel <Sam.Gannon@sportssouth.biz>
2024-05-14 10:05:00 -0500

    // repository implemenation

--- ExerciseTracker.samggannon/ExerciseTracker.samggannon/Data/Repositories/IRepository.cs
+++ ExerciseTracker.samggannon/ExerciseTracker.samggannon/Data/Repositories/IRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ExerciseTracker.samggannon.Data.Repositories
 {
-    internal class IRepository
+    public interface IRepository<T>
     {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
--- ExerciseTracker.samggannon/ExerciseTracker.samggannon/Data/Repositories/Repository.cs
+++ ExerciseTracker.samggannon/ExerciseTracker.samggannon/Data/Repositories/Repository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,40 @@ using System.Threading.Tasks;
 
 namespace ExerciseTracker.samggannon.Data.Repositories
 {
-    internal class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Update(entity);
+        }
     }
-}
+} 
