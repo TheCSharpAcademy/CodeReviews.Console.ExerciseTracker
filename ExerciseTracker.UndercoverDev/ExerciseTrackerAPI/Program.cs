2df7605c41f0505ab886fc2d2d9ec25fcb96f7b8
N-Endy <okafornel@gmail.com>
2024-08-16 13:03:59 +0100

    Add Services for exercise

--- ExerciseTracker.UndercoverDev/ExerciseTrackerAPI/Program.cs
+++ ExerciseTracker.UndercoverDev/ExerciseTrackerAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ExerciseTrackerAPI.Data;
+using ExerciseTrackerAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRepository, Repository>();
 
 builder.Services.AddDbContext<ExerciseTrackerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
