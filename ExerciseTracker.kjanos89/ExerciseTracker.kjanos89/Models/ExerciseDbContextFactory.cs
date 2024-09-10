3bc477716a0fabbc1f1fb5b63e143867c70520ef
kjanos89 <kjanos89@gmail.com>
2024-09-10 17:17:06 +0200

    Removal of unnecessary usings

--- ExerciseTracker.kjanos89/ExerciseTracker.kjanos89/Models/ExerciseDbContextFactory.cs
+++ ExerciseTracker.kjanos89/ExerciseTracker.kjanos89/Models/ExerciseDbContextFactory.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExerciseTracker.kjanos89.Models
 {
