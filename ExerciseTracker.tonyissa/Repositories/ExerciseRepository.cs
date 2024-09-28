de41e09ca5487ee80cc3742c2bb800a0893d7a45
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-09-28 15:49:53 -0400

    Add repository interface and implementation

---
+++ ExerciseTracker.tonyissa/Repositories/ExerciseRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExerciseTracker.tonyissa.Repositories;
+
+public class ExerciseRepository<T> : IExerciseRepository<T> where T : class, new()
+{
+    private readonly DbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public ExerciseRepository(DbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public T GetSessionById(int id)
+    {
+        return _dbSet.Find(id);
+    }
+
+    public IEnumerable<T> GetAllSessions()
+    {
+        return _dbSet.ToList<T>();
+    }
+
+    public void AddSession(T session)
+    {
+        _dbSet.Add(session);
+        _context.SaveChanges();
+    }
+
+    public void UpdateSession(T session)
+    {
+        _dbSet.Update(session);
+        _context.SaveChanges();
+    }
+
+    public void DeleteSession(T session)
+    {
+        _dbSet.Remove(session);
+        _context.SaveChanges();
+    }
+}
---
+++ ExerciseTracker.tonyissa/Repositories/IExerciseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ExerciseTracker.tonyissa.Repositories;
+
+interface IExerciseRepository<T>
+{
+    T GetSessionById(int id);
+    IEnumerable<T> GetAllSessions();
+    void AddSession(T session);
+    void DeleteSession(T session);
+    void UpdateSession(T session);
+}
