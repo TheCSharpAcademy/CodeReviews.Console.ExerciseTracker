320605995b8de04b84b17eb7bf5ed09b9d7cf298
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-09-29 15:58:51 -0400

    Checkpoint commit of UI implementation

---
+++ ExerciseTracker.tonyissa/UI/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using ExerciseTracker.tonyissa.Services;
+using Spectre.Console;
+
+namespace ExerciseTracker.tonyissa.UI;
+
+public class MainMenuController
+{
+    private readonly ExerciseService _service;
+
+    public MainMenuController(ExerciseService service)
+    {
+        _service = service;
+    }
+
+    public void StartMainMenu()
+    {
+        while (true)
+        {
+            var result = MenuSelection("What would you like to do?", MenuOptions.MainMenu);
+
+            switch (result)
+            {
+                case "View log":
+                    throw new NotImplementedException();
+                case "Add session":
+                    throw new NotImplementedException();
+                case "Remove session":
+                    throw new NotImplementedException();
+                case "Update session":
+                    throw new NotImplementedException();
+                default:
+                    return;
+            }
+        }
+    }
+
+    public static string MenuSelection(string title, string[] options)
+    {
+        var selection = AnsiConsole.Prompt<string>(
+            new SelectionPrompt<string>()
+                .Title(title)
+                .AddChoices(options)
+        );
+
+        return selection;
+    }
+}
---
+++ ExerciseTracker.tonyissa/UI/MainMenuOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace ExerciseTracker.tonyissa.UI;
+
+public static class MenuOptions
+{
+    public static string[] MainMenu = [
+        "View log",
+        "Add session",
+        "Remove session",
+        "Update session",
+        "Quit"
+    ];
+}
