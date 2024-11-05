08c4302c55c3161f480725ec630a8167944cb08d
hasona23 <130175104+hasona23@users.noreply.github.com>
2024-11-05 22:50:50 +0200

    Remove empty function

--- ExerciseTracker.hasona23/ExerciseTracker.hasona23/Data/ExerciseTrackerContext.cs
+++ ExerciseTracker.hasona23/ExerciseTracker.hasona23/Data/ExerciseTrackerContext.cs
@@ -7,11 +7,6 @@ public class ExerciseTrackerContext : DbContext
 {
        public DbSet<Exercise> Exercises { get ; set; }
 
-       protected override void OnModelCreating(ModelBuilder modelBuilder)
-       {
-              
-       }
-
        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
        {
               optionsBuilder.UseSqlServer(AppSettings.DefaultConnectionString);
